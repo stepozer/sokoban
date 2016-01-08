186d578150022781a4b47770e3c8e80459642e67
stepozer <stepozer@github.com>
2016-01-08 11:23:10 +0300

    display level packs

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,8 +1,15 @@
 class PlayController < ApplicationController
   def index
+    @level_packs = LevelPack.order(author: :asc, name: :asc)
+  end
+
+  def pack
+    @level_pack = LevelPack.find(params[:pack])
+    @levels = Level.where(level_pack: @level_pack).order(id: :asc)
   end
 
   def show
-    gon.push(sokoban_level: Level.find(params[:id]).level)
+    @level = Level.find(params[:level])
+    gon.push(sokoban_level: @level.level)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'play/:id', to: 'play#show',  as: 'play_show'
-  get 'play',     to: 'play#index', as: 'play_index'
-  get 'help',     to: 'pages#help', as: 'pages_help'
+  get 'play/:pack/:level',   to: 'play#show',  as: 'play_show'
+  get 'play/:pack',          to: 'play#pack',  as: 'play_show_pack'
+  get 'play',                to: 'play#index', as: 'play_index'
+  get 'help',                to: 'pages#help', as: 'pages_help'
   root 'pages#home'
 end
